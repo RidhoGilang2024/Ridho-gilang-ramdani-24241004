2d0a7375c7d07bcac3895955b30cda6917b92652
RidhoGilang2024 <ridhogilangramdani11@gmail.com>
2024-11-10 20:17:24 -0800

    modul 3

---
+++ ridho gilang ramdani/03_CastingTipeData/main.py
@@ -0,0 +1,29 @@
+# casting adalah cara ubah tipe data variabel
+
+# INTERGER
+print("===INTERGER===")
+data_int = 9
+print("data = ",data_int,",type = ",type(data_int))
+
+# casting
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data = ",data_float,"tipe data = ",type(data_float))
+print("data = ",data_str,"tipe data = ",type(data_str))
+print("data = ",data_bool,"tipe data = ",type(data_bool))
+
+
+# float
+print("===FLOAT===")
+float_data =3,14
+print("data = ",float_data,"tipe data = ",type(float_data))
+
+# casting
+# ubah menjadi int
+
+# ubah menjadi str
+
+# ubah menjadi bool
+
+# cetak
